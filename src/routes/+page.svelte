8383b4dc2acac2e40c381aca05be48a83ec8b827
Fabio Riva <f.riva@sotefin.ch>
2024-09-12 17:10:05 +0200

    page for picamera video streaming

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,12 @@
 <script>
   import { getContext } from "svelte";
-  import FaceId from "$lib/FaceId.svelte";
+  // import FaceId from "$lib/FaceId.svelte";
   import Gate from "$lib/Gate.svelte";
   import Pin from "$lib/Pin.svelte";
   import Start from "$lib/Start.svelte";
   // import Events from "$lib/Events.svelte";
+
   const { pageNr } = getContext("comm-context");
-  // console.log($pageNr);
-  // export let page;
 </script>
 
 {#if $pageNr === 1}
@@ -16,8 +15,19 @@
   <Gate />
 {:else if $pageNr === 3}
   <Pin />
+{:else if $pageNr === 4}
+  <!-- <FaceId /> -->
+  <div>
+    <p>picamera video streaming</p>
+    <iframe
+      src="http://192.168.20.201:5000"
+      title="picamera video streaming"
+      width="360"
+      height="280"
+      style="margin: auto;"
+    ></iframe>
+  </div>
 {:else}
-  <!-- <div>Page {$pageNr}</div> -->
+  <div>Page {$pageNr}</div>
   <!-- <Events /> -->
-  <FaceId />
 {/if}
