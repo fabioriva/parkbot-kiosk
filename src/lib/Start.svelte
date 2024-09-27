f69f24863cb5574c0612fc983690625a7c8c8e8e
Fabio Riva <f.riva@sotefin.ch>
2024-09-27 11:12:21 +0200

    new finger down image

--- src/lib/Start.svelte
+++ src/lib/Start.svelte
@@ -2,51 +2,17 @@
   import { t } from "$lib/i18n";
   import Text from "$lib/Text.svelte";
 
-  // let time = 0;
-  // let duration;
-  // let paused = true;
-  // let lastMouseDown;
-
-  // function handleMousedown(e) {
-  // 	lastMouseDown = new Date();
-  // }
-  // function handleMouseup(e) {
-  // 	if (new Date() - lastMouseDown < 300) {
-  // 		if (paused) e.target.play();
-  // 		else e.target.pause();
-  // 	}
-  // }
+  // const VIDEO_SRC = "seattle.mp4"
+  const VIDEO_SRC = "https://sveltejs.github.io/assets/caminandes-llamigos.mp4";
 </script>
 
-<video autoplay loop muted src="seattle.mp4" class="px-3">
-  <!-- <track kind="captions" /> -->
-</video>
-
-<!-- <h1>Caminandes: Llamigos</h1> -->
-<!-- <p>From <a href="https://studio.blender.org/films">Blender Studio</a>. CC-BY</p> -->
-<!-- <video
-	poster="https://sveltejs.github.io/assets/caminandes-llamigos.jpg"
-	src="https://sveltejs.github.io/assets/caminandes-llamigos.mp4"
-	on:mousedown={handleMousedown}
-	on:mouseup={handleMouseup}
->
-	<track kind="captions" />
-</video> -->
-
-<!-- <iframe
-	width="480"
-	height="300"
-	src="https://www.youtube.com/embed/YRO6r1mmBwU"
-	title="Seattle Automated Cars Parking"
-	frameborder="0"
-	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-	referrerpolicy="strict-origin-when-cross-origin"
-	allowfullscreen
-></iframe> -->
-<div class="mt-16 flex flex-col space-y-1">
-  <Text text={$t("start.text1")} />
-  <Text text={$t("start.text2")} />
-  <!-- <Text text={$t("ùstart.text3")} /> -->
+<div class="space-y-8">
+  <video autoplay loop muted src={VIDEO_SRC} class="px-1" />
+  <div class="flex flex-col">
+    <Text text={$t("start.text1")} />
+    <Text text={$t("start.text2")} />
+  </div>
+  <!-- <div class="text-9xl">&#9759;</div> -->
+  <!-- <div class="text-6xl">&darr;</div> -->
+  <img src="icons8-finger-down-100.png" alt="finger-down" class="mx-auto" />
 </div>
-<div class="text-9xl">&#9759;</div>
-<!-- <div class="text-6xl">&darr;</div> -->
