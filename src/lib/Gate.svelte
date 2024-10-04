fd439bc5f8bb104457edda94937ac8200a77b743
Fabio Riva <f.riva@sotefin.ch>
2024-10-04 11:25:20 +0200

    try to set button active state

--- src/lib/Gate.svelte
+++ src/lib/Gate.svelte
@@ -21,7 +21,7 @@
 
   onMount(() => {
     let btn = document.getElementById("btn-close");
-    btn?.addEventListener("click", setActiveState);
+    btn?.addEventListener("touchstart", setActiveState);
     console.log(btn);
     function setActiveState() {
       console.log("setActiveState");
@@ -48,5 +48,5 @@
   id="btn-close"
   class="w-[256px] h-[256px] rounded-full bg-yellow-500 border-4 border-yellow-600 opacity-100 shadow-2xl shadow-slate-700 active:bg-gradient-to-br from-amber-500 to-amber-600 font-bold text-2xl text-amber-700 active:text-amber-600"
   on:touchend={handleTouchEnd}
-  on:click={handleTouchStart}>PUSH TO CLOSE</button
+  on:touchstart={handleTouchStart}>PUSH TO CLOSE</button
 >
