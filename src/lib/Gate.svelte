ef16b8c282feb20347cead8ddca0f525cb37c9ab
Fabio Riva <f.riva@sotefin.ch>
2024-10-04 10:50:05 +0200

    improved page ui

--- src/lib/Gate.svelte
+++ src/lib/Gate.svelte
@@ -1,37 +1,26 @@
 <script>
   // import { PUBLIC_API } from "$env/static/public";
-  import { getContext, onMount } from "svelte";
+  import { getContext } from "svelte";
   import { t } from "$lib/i18n";
   import Text from "$lib/Text.svelte";
 
   const data = getContext("data-context");
 
-  async function handleTouchEnd(e) {
-    // console.log(event, 'UN-PRESSED');
+  async function handleTouchEnd() {
+    // e.preventDefault();
     // const url = PUBLIC_API + "/unpress"
     // data = await fetch("/api/unpress").then((x) => x.json());
     await fetch("/api/unpress");
   }
-  async function handleTouchStart(e) {
-    // console.log(event, 'PRESSED');
+  async function handleTouchStart() {
+    // e.preventDefault();
     // const url = PUBLIC_API + "/press"
     // data = await fetch("/api/press").then((x) => x.json());
     await fetch("/api/press");
   }
-
-  // onMount(() => {
-  //   const button = document.getElementById("close"); // select the button element
-  //   button.addEventListener("click", function () {
-  //     button.classList.toggle("active:shadow-slate-900"); // toggle the 'selected' class on click
-  //   });
-  // });
 </script>
 
-<div class="flex flex-col space-y-16">
-  <!-- <Text text={$t("gate.text1")} /> -->
-  <!-- <Text text={$t("gate.text2")} /> -->
-  <Text text={$t("gate.text0")} />
-
+<div class="mb-8">
   {#if $data.successMesg === 1}
     <Text text={$t("gate.text1")} />
   {:else if $data.successMesg === 2}
@@ -40,13 +29,13 @@
     <Text text={$t("gate.text3")} />
   {:else if $data.successMesg === 4}
     <Text text={$t("gate.text4")} />
+  {:else}
+    <Text text="UNKNOWN" />
   {/if}
 </div>
 
 <button
-  id="close"
-  type="button"
-  class="rounded-full my-16 p-32 bg-amber-500 bg-gradient-to-br from-amber-300 via-amber-500 active:bg-amber-700 shadow-2xl shadow-slate-600 active:shadow-slate-900"
+  class="w-[256px] h-[256px] rounded-full bg-yellow-500 border-4 border-yellow-600 opacity-100 shadow-2xl shadow-slate-700 active:bg-gradient-to-br from-amber-500 to-amber-600 font-bold text-2xl text-amber-700 active:text-amber-600"
   on:touchend={handleTouchEnd}
-  on:touchstart={handleTouchStart}
-/>
+  on:touchstart={handleTouchStart}>PUSH TO CLOSE</button
+>
