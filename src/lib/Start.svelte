1ca8faafc7e6c1494be44309d58a84655e32af78
Fabio Riva <f.riva@sotefin.ch>
2024-10-09 15:08:19 +0200

    picamera face recognition video feed

--- src/lib/Start.svelte
+++ src/lib/Start.svelte
@@ -1,13 +1,22 @@
 <script>
+  import { PUBLIC_CAMERA_SRC } from "$env/static/public";
   import { t } from "$lib/i18n";
   import Text from "$lib/Text.svelte";
 
   // const VIDEO_SRC = "seattle.mp4"
-  const VIDEO_SRC = "https://sveltejs.github.io/assets/caminandes-llamigos.mp4";
+  // const VIDEO_SRC = "https://sveltejs.github.io/assets/caminandes-llamigos.mp4";
 </script>
 
 <div class="space-y-8">
-  <video autoplay loop muted src={VIDEO_SRC} class="px-1" />
+  <!-- <video autoplay loop muted src={VIDEO_SRC} class="px-1" /> -->
+  <iframe
+    src={PUBLIC_CAMERA_SRC}
+    title="picamera video streaming"
+    width="480"
+    height="270"
+    scrolling="no"
+    style="margin: auto;"
+  ></iframe>
   <div class="flex flex-col">
     <Text text={$t("start.text1")} />
     <Text text={$t("start.text2")} />
