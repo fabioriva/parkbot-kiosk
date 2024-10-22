0cecbba5b6910ce94d648f1994f80953cde5fdfc
Fabio Riva <f.riva@sotefin.ch>
2024-10-22 15:22:24 +0200

    new faceid page

--- src/lib/Start.svelte
+++ src/lib/Start.svelte
@@ -1,15 +1,23 @@
 <script>
-  import { PUBLIC_CAMERA_SRC } from "$env/static/public";
+  // import { PUBLIC_CAMERA_SRC } from "$env/static/public";
   import { t } from "$lib/i18n";
   import Text from "$lib/Text.svelte";
 
   // const VIDEO_SRC = "seattle.mp4"
-  // const VIDEO_SRC = "https://sveltejs.github.io/assets/caminandes-llamigos.mp4";
+  const VIDEO_SRC = "https://sveltejs.github.io/assets/caminandes-llamigos.mp4";
 </script>
 
 <div class="space-y-8">
-  <!-- <video autoplay loop muted src={VIDEO_SRC} class="px-1" /> -->
-  <iframe
+  <video
+    autoplay
+    loop
+    muted
+    src={VIDEO_SRC}
+    class="px-0"
+    width="480"
+    height="360"
+  />
+  <!-- <iframe
     src={PUBLIC_CAMERA_SRC}
     title="picamera video streaming"
     width="480"
@@ -18,7 +26,7 @@
     marginwidth="0"
     marginheight="0"
     frameborder="0"
-  ></iframe>
+  ></iframe> -->
   <div class="flex flex-col">
     <Text text={$t("start.text1")} />
     <Text text={$t("start.text2")} />
---
+++ src/routes/faceid/+page.svelte
@@ -0,0 +1,82 @@
+<script>
+  import { PUBLIC_CAMERA_SRC } from "$env/static/public";
+  import { onMount } from "svelte";
+
+  // onMount(() => {
+  //   const width = 320; // We will scale the photo width to this
+  //   let height = 0; // This will be computed based on the input stream
+
+  //   video = document.getElementById("video");
+  //   canvas = document.getElementById("canvas");
+  //   photo = document.getElementById("photo");
+
+  //   function clearphoto() {
+  //     const context = canvas.getContext("2d");
+  //     context.fillStyle = "#AAA";
+  //     context.fillRect(0, 0, canvas.width, canvas.height);
+
+  //     const data = canvas.toDataURL("image/png");
+  //     photo.setAttribute("src", data);
+  //   }
+
+  //   function takepicture() {
+  //     const context = canvas.getContext("2d");
+  //     if (width && height) {
+  //       canvas.width = width;
+  //       canvas.height = height;
+  //       context.drawImage(video, 0, 0, width, height);
+
+  //       const data = canvas.toDataURL("image/png");
+  //       photo.setAttribute("src", data);
+  //     } else {
+  //       clearphoto();
+  //     }
+  //   }
+  // });
+</script>
+
+<div>
+  <p>picamera video streaming</p>
+  <iframe
+    id="video"
+    src={PUBLIC_CAMERA_SRC}
+    title="picamera video streaming"
+    width="480"
+    height="360"
+    scrolling="no"
+    marginwidth="0"
+    marginheight="0"
+    frameborder="0"
+  ></iframe>
+  <button
+    class="active:bg-green-500"
+    id="startbutton"
+    on:click={() => alert("clicked")}>Take photo</button
+  >
+</div>
+<canvas id="canvas"> </canvas>
+<div class="output">
+  <img id="photo" alt="The screen capture will appear in this box." />
+</div>
+
+<style>
+  #canvas {
+    display: none;
+  }
+  #startbutton {
+    display: block;
+    position: relative;
+    margin-left: auto;
+    margin-right: auto;
+    bottom: 32px;
+    background-color: rgb(0 150 0 / 50%);
+    border: 1px solid rgb(255 255 255 / 70%);
+    box-shadow: 0px 0px 1px 2px rgb(0 0 0 / 20%);
+    font-size: 14px;
+    font-family: "Lucida Grande", "Arial", sans-serif;
+    color: rgb(255 255 255 / 100%);
+    min-width: 120px;
+    min-height: 40px;
+    /* top: 100px; */
+  }
+</style>
